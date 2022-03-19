2108887c483ff1ba48f25580932f11d9a763c520
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-19 22:03:10 +0800

    Delete bmarks directory

--- bmarks/microbench/linkedlist/intset.h
+++
@@ -1,6 +0,0 @@
-#include "linkedlist.h"
-
-int set_contains(intset_t *set, val_t val, int transactional);
-int set_add(intset_t *set, val_t val, int transactional);
-int set_remove(intset_t *set, val_t val, int transactional);
-
